7b4ff533a78889a4794b7572d7beb2343765c634
sigmalko <github@rodologia.pl>
2025-10-06 17:24:53 +0200

    Configure UTF-8 via Spring encoding properties

--- modules/export-to-gmail/src/main/java/com/github/sigmalko/protonmail/export/config/web/WebInfrastructureConfig.java
+++ modules/export-to-gmail/src/main/java/com/github/sigmalko/protonmail/export/config/web/WebInfrastructureConfig.java
@@ -6,25 +6,11 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
-import org.springframework.web.filter.CharacterEncodingFilter;
 
 import com.github.sigmalko.protonmail.export.web.filter.CORSFilter;
 
 @Configuration
 public class WebInfrastructureConfig {
-
-    @Bean
-    FilterRegistrationBean<CharacterEncodingFilter> characterEncodingFilterRegistration() {
-        final var filter = new CharacterEncodingFilter();
-        filter.setEncoding("UTF-8");
-        filter.setForceEncoding(true);
-        filter.setForceRequestEncoding(true);
-        filter.setForceResponseEncoding(false);
-
-        final var registrationBean = new FilterRegistrationBean<>(filter);
-        registrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE + 1);
-        return registrationBean;
-    }
 
     @Bean
     FilterRegistrationBean<CORSFilter> corsFilterRegistration(CORSFilter corsFilter) {
